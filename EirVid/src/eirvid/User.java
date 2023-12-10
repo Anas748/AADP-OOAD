e8eb936d94b4a959290d6f6afbab15d15af3c653
Mani8588 <moonrana173@yahoo.com>
2023-12-10 23:31:00 +0000

    updating User class

--- EirVid/src/eirvid/User.java
+++ EirVid/src/eirvid/User.java
@@ -9,6 +9,20 @@ package eirvid;
  * @author user
  */
 public class User {
+    private String password;
+    private String email;
+    private Map<Movie, Long> rentedMovies;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.rentedMovies = new HashMap<>();
+    }
+
+    public void rentMovie(Movie movie) {
+        rentedMovies.put(movie, System.currentTimeMillis());
+        System.out.printf("User: %s, Movie: %s, Price: %.2f\n", email, movie.getTitle(), movie.getPrice());
+    }
      
 }
 
