0a1663af7a3dbf339b733f51be66e0180f96a46d
Anas baig <100359811+Anas748@users.noreply.github.com>
2023-12-10 23:37:52 +0000

    Update EirVid.java created login function with all other functions in main

--- EirVid/src/eirvid/EirVid.java
+++ EirVid/src/eirvid/EirVid.java
@@ -19,8 +19,90 @@ public class EirVid {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        public List<Movie> recommendMovies() {
+        public static void main(String[] args) {
+        MovieRentalSystem system = MovieRentalSystem.getInstance();
+        String csvFilePath = "/Users/apple/Downloads/test/ij/untitled/src/main/java/org/example/Movie_Metadata.csv";
+        system.loadMovies(csvFilePath); // Update the CSV file name here
+
+        Scanner SC = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("\n\nEnter email (Enter 'X' to Exit): ");
+            String email = SC.nextLine();
+            if(Objects.equals(email, "X")) {
+                break;
+            }
+
+            System.out.print("Enter password: ");
+            String pass = SC.nextLine();
+
+            // Create a new user or log in if the user already exists
+            User loggedInUser = system.login(email, pass);
+            if (loggedInUser == null) {
+                // If the user does not exist, create a new user
+                system.createUser(email, pass);
+                loggedInUser = system.login(email, pass);
+            }
+
+            // Rest of the code remains the same
+            if (loggedInUser != null) {
+                Menu menu = new Menu(loggedInUser);
+                menu.displayMenu();
+            } else {
+                System.out.println("Invalid email or password.");
+            }
+        }
+    }
+
+    private static MovieRentalSystem instance;
+    private Map<String, User> users;
+    private List<Movie> movies;
+
+    private MovieRentalSystem() {
+        this.users = new HashMap<>();
+        this.movies = new ArrayList<>();
+    }
+
+    public static MovieRentalSystem getInstance() {
+        if (instance == null) {
+            instance = new MovieRentalSystem();
+        }
+        return instance;
+    }
+
+    public void addMovie(String title, double price) {
+        this.movies.add(new Movie(title, price));
+    }
+
+    public void createUser(String email, String password) {
+        this.users.put(email, new User(email, password));
+    }
+
+    public User login(String email, String password) {
+        User user = users.get(email);
+        if (user != null && password.equals(user.getPassword())) {
+            return user;
+        }
+        return null;
+    }
+
+    public void rentMovie(String email, String title) {
+        User user = this.users.get(email);
+        Movie movie = this.movies.stream().filter(m -> m.getTitle().equals(title.strip())).findFirst().orElse(null);
+
+        if (movie != null) {
+            user.rentMovie(movie);
+            System.out.println("x:-----------------Movie rented successfully!-----------------:x");
+            System.out.println("Movie: " + movie.getTitle());
+            System.out.println("Price: " + movie.getPrice());
+            System.out.println("Customer: " + user.getEmail());
+        } else {
+            System.out.println("Invalid movie title.");
+        }
+    }
+
+    public List<Movie> recommendMovies() {
         Map<Movie, Integer> movieRentCount = new HashMap<>();
         long fiveMinutesAgo = System.currentTimeMillis() - (5 * 60 * 1000);
 
@@ -32,6 +114,31 @@ public class EirVid {
                 }
             }
         }
+
+        return movieRentCount.entrySet().stream()
+                .sorted(Map.Entry.<Movie, Integer>comparingByValue().reversed())
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+         public void loadMovies(String csvFilePath) {
+        try {
+            Scanner sc = new Scanner(new FileReader(csvFilePath));
+            // String[] nextLine;
+            sc.useDelimiter(",");
+            sc.nextLine(); // skip the first line
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] parts = line.split(",");
+                String title = parts[1];
+                double price = Double.parseDouble(parts[parts.length - 1]);
+                addMovie(title.strip(), price);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+       
     
     
 
