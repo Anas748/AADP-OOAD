b80e825e65f8865d228d5009713959645332b9ae
Haider007A <100359869+Haider007A@users.noreply.github.com>
2023-12-10 23:31:49 +0000

    Created Movie recommend function in main

--- EirVid/src/eirvid/EirVid.java
+++ EirVid/src/eirvid/EirVid.java
@@ -4,6 +4,10 @@
  */
 package eirvid;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *
  * @author user
@@ -15,6 +19,19 @@ public class EirVid {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        
+        public List<Movie> recommendMovies() {
+        Map<Movie, Integer> movieRentCount = new HashMap<>();
+        long fiveMinutesAgo = System.currentTimeMillis() - (5 * 60 * 1000);
+
+        for (User user : users.values()) {
+            Map<Movie, Long> rentedMovies = user.getRentedMoviesWithTime();
+            for (Movie movie : rentedMovies.keySet()) {
+                if (rentedMovies.get(movie) >= fiveMinutesAgo) {
+                    movieRentCount.put(movie, movieRentCount.getOrDefault(movie, 0) + 1);
+                }
+            }
+        }
     
-}
+    
+
