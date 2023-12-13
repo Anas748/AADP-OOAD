83cd7a25261416e4831d2edd3840287dd93a6a11
Ahmad <2021385@student.cct.ie>
2023-12-13 01:43:08 +0000

    Created the MovieRentalManager class with basic structure, including a  instance, a map and a list for movies.

---
+++ EirVid/src/eirvid/MovieRentalManager.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eirvid;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @Muhammad Ahmad 2021385
+ */
+public class MovieRentalManager {
+    private static MovieRentalManager instance;
+    private Map<String, User> users;
+    private List<Movie> movies;
+
+    private MovieRentalManager() {
+        this.users = new HashMap<>();
+        this.movies = new ArrayList<>();
+    }
+}
