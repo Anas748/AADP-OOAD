fb61f221e9001151034a5a19a4b542c7334bbffd
Haider007A <100359869+Haider007A@users.noreply.github.com>
2023-12-13 02:17:20 +0000

    Add movieloader class as a perser for csvreader

--- EirVid/src/eirvid/MovieLoader.java
+++ EirVid/src/eirvid/MovieLoader.java
@@ -4,10 +4,34 @@
  */
 package eirvid;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
- * @author PC
+ * @author Haider
  */
 public class MovieLoader {
-    
+
+    public static void loadMovies() {
+        String csvFilePath = CsvReader.readCsvFilePath();
+
+        try {
+            Scanner sc = new Scanner(new FileReader(csvFilePath));
+            sc.useDelimiter(",");
+            sc.nextLine(); // skip the first line
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                String[] parts = line.split(",");
+                String title = parts[1];
+                double price = Double.parseDouble(parts[parts.length - 1]);
+                
+                System.out.println("Add Movie: " + title.trim() + ", Price: " + price);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+       
