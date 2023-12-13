2ba25d618b4afee4a1cc307b5549c5e8fa5017c2
Haider007A <100359869+Haider007A@users.noreply.github.com>
2023-12-13 23:54:14 +0000

    Created class for movie rental manager

---
+++ EirVid/src/eirvid/MovieRentalManager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eirvid;
+
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+class MovieRentalManager {
+
+    static MovieRentalManager getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    List<Movie> getMovies() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
