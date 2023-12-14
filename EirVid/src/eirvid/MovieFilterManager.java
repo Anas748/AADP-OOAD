1358173712a4aa5caaf4736ec8176370db40b609
Ahmad-385 <100360026+Ahmad-385@users.noreply.github.com>
2023-12-14 16:22:22 +0000

    Update MovieFilterManager.java

--- EirVid/src/eirvid/MovieFilterManager.java
+++ EirVid/src/eirvid/MovieFilterManager.java
@@ -6,8 +6,38 @@ package eirvid;
 
 /**
  *
- * @author PC
+ * @author Ahmad
  */
 public class MovieFilterManager {
+     private List<Movie> movies;
+
+    //Constructor to Initializes the MovieFilterManager with a list of movies.
+        public MovieFilterManager(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+   
+    public List<Movie> filterMovies(String preference) {
+        return this.movies.stream()
+                .filter(movie -> isMovieMatchingPreference(movie, preference))
+                .collect(Collectors.toList());
+    }
+
     
+      //Checks if a movie matches the  preference of users.
+     
+    //  The user's movie preference are ("old", "popular", "new").
+     
+    private boolean isMovieMatchingPreference(Movie movie, String preference) {
+        switch (preference.toLowerCase()) {
+            case "old":
+                return movie.getPrice() == 6.99;
+            case "popular":
+                return movie.getPrice() == 7.99;
+            case "new":
+                return movie.getPrice() == 9.99;
+            default:
+                return false; // Default behavior if preference is not recognized
+        }
+    }
 }
