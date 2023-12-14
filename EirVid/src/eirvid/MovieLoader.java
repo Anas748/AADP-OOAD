4d4bb0d549791253609c5ce8fa2c907c327ca49c
Haider007A <100359869+Haider007A@users.noreply.github.com>
2023-12-14 00:08:42 +0000

    updated movieloader class

--- EirVid/src/eirvid/MovieLoader.java
+++ EirVid/src/eirvid/MovieLoader.java
@@ -14,19 +14,27 @@ import java.util.Scanner;
  */
 public class MovieLoader {
 
-    public static void loadMovies() {
+    public static void loadMovies(MovieRentalManager manager) {
+        //use string for csv file path to read file from CsvReader class file
         String csvFilePath = CsvReader.readCsvFilePath();
 
         try {
+            //use scanner to read file
             Scanner sc = new Scanner(new FileReader(csvFilePath));
             sc.useDelimiter(",");
-            sc.nextLine(); // skip the first line
+            
+            // skip the first line
+            sc.nextLine(); 
+            
             while (sc.hasNext()) {
                 String line = sc.nextLine();
                 String[] parts = line.split(",");
                 String title = parts[1];
+                
+                //use double for the price as decimal
                 double price = Double.parseDouble(parts[parts.length - 1]);
                 
+                //print the title and price of added movie
                 System.out.println("Add Movie: " + title.trim() + ", Price: " + price);
             }
         } catch (IOException e) {
