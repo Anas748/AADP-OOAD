4c96c4dd2c67d65869cd766dc0f459ba71ff693e
Ahmad-385 <100360026+Ahmad-385@users.noreply.github.com>
2023-12-14 16:29:35 +0000

    Update MovieAdder.java

--- EirVid/src/eirvid/MovieAdder.java
+++ EirVid/src/eirvid/MovieAdder.java
@@ -6,8 +6,21 @@ package eirvid;
 
 /**
  *
- * @author PC
+ * @author Muhammad Ahmad
+ *MovieAdder, is responsible for adding new movies to the system
  */
 public class MovieAdder {
+    private List<Movie> movies;  // List to store movie data
+
+    
+    public MovieAdder(List<Movie> movies) {         //Initializes the MovieAdder with a list of movies.
+        this.movies = movies;                       //movies List of movies in the system.
+    }
+
+    
+    
+    public void addMovie(String title, double price) {      //Adds a new movie to the system with the specified title and price.
+        this.movies.add(new Movie(title, price));           //Title & price of the new movie.
+    } 
     
 }
