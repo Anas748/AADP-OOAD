e859ff4a856315b98535c11d7fb44775236f0022
Ahmad <2021385@student.cct.ie>
2023-12-14 16:34:48 +0000

    CreatindRenntMovie class

---
+++ EirVid/src/eirvid/RentMovie.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eirvid;
+
+import java.util.List;
+
+/**
+ *
+ * @author Muhammad Ahmad 2021385
+ * RentalManager, manages the movie rental process by interacting with user and movie data.
+ */
+public class RentMovie {
+
+    RentMovie(List<User> users, List<Movie> movies) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void rentMovie(String email, String title) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
