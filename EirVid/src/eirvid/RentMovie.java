55a0c5df2e5a48edbcb0eac473fdd9c1bcd33c23
Ahmad-385 <100360026+Ahmad-385@users.noreply.github.com>
2023-12-14 16:56:45 +0000

    Update RentMovie.java

--- EirVid/src/eirvid/RentMovie.java
+++ EirVid/src/eirvid/RentMovie.java
@@ -12,13 +12,39 @@ import java.util.List;
  * RentalManager, manages the movie rental process by interacting with user and movie data.
  */
 public class RentMovie {
+    private List<User> users;   // List to store user data
+    private List<Movie> movies; // List to store movie data
 
-    RentMovie(List<User> users, List<Movie> movies) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    // Initializes the RentalManager with lists of users and movies.
+    public RentMovie(List<User> users, List<Movie> movies) {
+        this.users = users;
+        this.movies = movies;
     }
 
-    void rentMovie(String email, String title) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    // Initiates the movie rental process for a given user and movie title.
+    public void rentMovie(String email, String title) {
+        User user = this.users.stream()
+                .filter(u -> u.getEmail().equalsIgnoreCase(email))
+                .findFirst()
+                .orElse(null);
+
+        Movie movie = this.movies.stream()
+                .filter(m -> m.getTitle().equals(title.strip()))
+                .findFirst()
+                .orElse(null);
+
+        // Check if both user and movie are found
+        if (user != null && movie != null) {
+            // Rent the movie for the user
+            user.rentMovie(movie);
+
+            // Display rental information
+            System.out.println("x:--HUURRRAAAHHH! /n Movie rented successfully!----");
+            System.out.println("Movie: " + movie.getTitle());
+            System.out.println("Price: " + movie.getPrice());
+            System.out.println("Customer: " + user.getEmail());
+        } else {
+            System.out.println("Invalid email or movie title.");
+        }
     }
-    
 }
