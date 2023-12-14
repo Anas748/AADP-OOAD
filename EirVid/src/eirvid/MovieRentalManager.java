a3754131a8a3e6126ce47eee527dc2bd10ff87b0
Ahmad <2021385@student.cct.ie>
2023-12-14 15:36:12 +0000

    Updating MovieRentalManager class

--- EirVid/src/eirvid/MovieRentalManager.java
+++ EirVid/src/eirvid/MovieRentalManager.java
@@ -1,33 +1,69 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package eirvid;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  *
- * @Muhammad Ahmad 2021385
+ * @Muhammad Ahmad
+ * 2021385
+ *  MovieRentalManager class manages the overall movie rental system.
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MovieRentalManager {
-    private static MovieRentalManager instance;
-
-    static MovieRentalManager getInstance() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    private static MovieRentalManager instance;       // Singleton instance to ensure only one instance of MovieRentalManager exists.
+    
+    // Lists to store users, movies, and instances of related managers.
+    private final List<User> users;
+    private final List<Movie> movies;
+    private final MovieFilterManager movieFilterManager;
+    private final MovieAdder movieAdder;
+    private final RentalManager rentalManager;
+      
+    
+    private MovieRentalManager() {                    // Private constructor initializes lists and manager instances.
+        this.users = new ArrayList<>();
+        this.movies = new ArrayList<>();
+        this.movieFilterManager = new MovieFilterManager(this.movies);
+        this.movieAdder = new MovieAdder(this.movies);
+        this.rentalManager = new RentMovie(this.users, this.movies);
     }
-    private Map<String, User> users;
-    private List<Movie> movies;
 
-    private MovieRentalManager() {
-        this.users = new HashMap<>();
-        this.movies = new ArrayList<>(); 
+    
+    public static MovieRentalManager getInstance() {      //returns the instance of MovieRentalManager.
+        if (instance == null) {
+            instance = new MovieRentalManager();
+        }
+        return instance;
     }
 
-    List<Movie> getMovies() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<User> getUsers() {                        // Getter method to retrieve a copy of the user list.
+
+        return new ArrayList<>(users);
     }
-}
+
+   
+    public List<Movie> getMovies() {                      // Getter method to retrieve a copy of the movie list.
+        return new ArrayList<>(movies);
+    }
+
+    
+    public List<Movie> filterMovies(String preference) {     // Method to filter movies based on a user's preference using the MovieFilterManager.
+        return movieFilterManager.filterMovies(preference);
+    }
+
+    
+    public void addMovie(String title, double price) {     // Method to add a new movie using the MovieAdder.
+        movieAdder.addMovie(title, price);
+    }
+
+    
+    public void RentMovie(String email, String title) {      // Method to rent a movie for a user using the RentalManager.
+       
